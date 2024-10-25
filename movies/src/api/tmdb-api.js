93da0cea996aae8cd861502d75cd8d6be5ce5f83
reifionn <20101977@mail.wit.ie>
2024-10-25 16:54:49 +0100

    Attempting to add actors roles, similar to movie reviews

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -161,3 +161,19 @@ export const getActorImages = ({ queryKey }) => {
     throw error
   });
 };
+
+export const getActorRoles = (args) => {
+  const [, idPart] = args.queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error;
+  });
+};
--- movies/src/components/actorDetails/index.js
+++ movies/src/components/actorDetails/index.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
-import StarRate from "@mui/icons-material/StarRate";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import CakeIcon from "@mui/icons-material/Cake";
+import PlaceIcon from "@mui/icons-material/Place";
+import PopularityIcon from "@mui/icons-material/Whatshot";
+import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
-import FaceIcon from "@mui/icons-material/Face";
+import Drawer from "@mui/material/Drawer";
+import PersonIcon from "@mui/icons-material/Person";
+import ActorRoles from "../actorRoles";
 
 
 const root = {
@@ -29,19 +35,55 @@ const ActorDetails = ({ actor }) => {  // Don't miss this!
         Name: {actor.name}
       </Typography>
 
+      <Typography variant="h6" component="p">
+        Biography: { actor.biography || "Biography not available." }
+      </Typography>
+
       <Paper component="ul" sx={{ ...root }}>
-        <Chip
-          icon={<StarRate />}
-          label={`Popularity: ${actor.popularity}`}
-          sx={{ ...chip }}
-        />
-        <Chip label={`Gender: ${actor.gender === 1 ? "Female" : "Male"}`} sx={{ ...chip }} />
-        <Chip
-          label={`Known For: ${actor.known_for_department}`}
-          sx={{ ...chip }}
-          icon={<FaceIcon />}
-        />
+                <Chip icon={<CakeIcon />} label={`Born: ${actor.birthday || "Unknown"}`} />
+                {actor.deathday && (
+                    <Chip icon={<AccessTimeIcon />} label={`Died: ${actor.deathday}`} />
+                )}
+                <Chip
+                    icon={<PlaceIcon />}
+                    label={`Place of Birth: ${actor.place_of_birth || "Unknown"}`}
+                />
+                <Chip icon={<PopularityIcon />} label={`Popularity: ${actor.popularity.toFixed(1)}`} />
+                <Chip label={`Department: ${actor.known_for_department || "N/A"}`} />
       </Paper>
+
+      <Paper component="ul" sx={{ ...root }}>
+                <li>
+                    <Chip label="Also Known As" sx={{ ...chip }} color="primary" />
+                </li>
+                {actor.also_known_as && actor.also_known_as.length > 0 ? (
+                    actor.also_known_as.map((aka, index) => (
+                        <li key={index}>
+                            <Chip label={aka} sx={{ ...chip }} />
+                        </li>
+                    ))
+                ) : (
+                    <Chip label="No alternative names" sx={{ ...chip }} />
+                )}
+      </Paper>
+
+      <Fab
+          color="secondary"
+          variant="extended"
+          onClick={() => setDrawerOpen(true)}
+          sx={{
+              position: 'fixed',
+              bottom: '1em',
+              right: '1em'
+          }}
+      >
+          <PersonIcon />
+          Known Roles
+      </Fab>
+
+      <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+          <ActorRoles actorId={actor.id} />
+      </Drawer>
 
       </>
   );
