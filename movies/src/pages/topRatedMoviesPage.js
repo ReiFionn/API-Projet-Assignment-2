85aadef8a1e5b8cd602f2710534a1f71070d807a
reifionn <20101977@mail.wit.ie>
2024-11-08 16:50:53 +0000

    Added pagination to top rated (i think)

--- movies/src/pages/topRatedMoviesPage.js
+++ movies/src/pages/topRatedMoviesPage.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { getTopRatedMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
 import AddToMustWatchIcon from '../components/cardIcons/addToMustWatch';
 import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
+import { Pagination } from "@mui/material";
 
 const TopRatedMoviesPage = (props) => {
-
-  const {  data, error, isLoading, isError }  = useQuery('top_rated', getTopRatedMovies)
+  const [currentPage, setCurrentPage] = useState(1);
+  const {  data, error, isLoading, isError }  = useQuery(['top_rated', currentPage], () => getTopRatedMovies(currentPage))
 
   if (isLoading) {
     return <Spinner />
@@ -20,12 +21,19 @@ const TopRatedMoviesPage = (props) => {
   
   const movies = data.results;
 
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
+
+  const totalPages = Math.ceil(data.total_results);
+
   // Redundant, but necessary to avoid app crashing.
   const mustWatch = movies.filter(m => m.mustWatch)
   localStorage.setItem('mustWatch', JSON.stringify(mustWatch))
   const addToMustWatch = (movieId) => true 
 
   return (
+    <>
     <PageTemplate
       title="Top Rated Movies"
       movies={movies}
@@ -36,6 +44,15 @@ const TopRatedMoviesPage = (props) => {
         </>
       }}
     />
+    <Pagination
+        style={{ marginTop: '25px', display: 'flex', justifyContent: 'center' }}
+        count={totalPages}
+        color="secondary"
+        onChange={handlePageChange}
+        page={currentPage}
+        size="large"
+    />
+    </>
 );
 };
 export default TopRatedMoviesPage;
